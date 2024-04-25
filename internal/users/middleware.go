90c5384282e03960fe4c28e009f3dc1a198889ba
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-04-25 22:03:20 +0400

    add dummy middleware for Telegram user auth

---
+++ internal/users/middleware.go
@@ -0,0 +1,18 @@
+package telegram_users
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+// makeTelegramAuthUserMiddleware constructs a middleware which is responsible for
+// Telegram user auth.
+func makeTelegramAuthUserMiddleware() endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (interface{}, error) {
+			// TODO: Implement the authentication middleware
+			return next(ctx, request)
+		}
+	}
+}
