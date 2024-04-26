94be5eeb254815f66636d48a8953e929224195ec
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-04-27 00:20:05 +0400

    enhance endpoint logging

--- internal/users/endpoint.go
+++ internal/users/endpoint.go
@@ -2,8 +2,10 @@ package telegram_users
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/go-kit/kit/endpoint"
+	"go.uber.org/zap"
 )
 
 // makeCreateOrUpdateTgUserEndpoint constructs a CreateOrUpdateTgUser endpoint wrapping the service.
@@ -11,13 +13,21 @@ import (
 // Path: PUT /api/v1/public/auth/telegram
 func makeCreateOrUpdateTgUserEndpoint(s *Service) endpoint.Endpoint {
 	return func(ctx context.Context, _ interface{}) (interface{}, error) {
-		user, err := GetUserFromContext(ctx)
+		usr, err := GetUserFromContext(ctx)
 		if err != nil {
+			s.log.With(
+				zap.String("method", "GetUserFromContext"),
+				zap.String("external_id", strconv.FormatInt(usr.ExternalId, 10)),
+			).Error(err.Error())
 			return nil, err
 		}
 
-		v0, err := s.CreateOrUpdateTgUser(ctx, CreateOrUpdateTgUserRequest(user))
+		v0, err := s.CreateOrUpdateTgUser(ctx, CreateOrUpdateTgUserRequest(usr))
 		if err != nil {
+			s.log.With(
+				zap.String("method", "s.repo.CreateOrUpdateTgUser"),
+				zap.String("external_id", strconv.FormatInt(usr.ExternalId, 10)),
+			).Error(err.Error())
 			return nil, err
 		}
 		return v0, nil
