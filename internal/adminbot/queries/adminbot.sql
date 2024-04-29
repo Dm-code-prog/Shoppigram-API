c9ebe90bfdfb4a9a0d111284312b646d9a73a146
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-04-29 23:42:04 +0400

    add DDL and DML related stuff

---
+++ internal/adminbot/queries/adminbot.sql
@@ -0,0 +1,9 @@
+-- GetAdminUsersList :many
+select admin_username
+from notify_list
+where web_app_id = $1;
+
+-- GetAdminUserBotToken :one
+select pgp_sym_decrypt(admin_bot_encr_token, @encryption_key::text)
+from web_apps
+where id = $1;
---
+++ internal/adminbot/schemas/20240429.sql
@@ -0,0 +1,12 @@
+-- Notifiers
+create table notifier_cursors (
+  last_processed_created_at timestamp primary key,
+  last_processed_id uuid references orders(id),
+  name varchar(50) unique
+);
+
+-- Notify list
+create table notify_list (
+	web_app_id uuid references web_apps(id),
+  admin_username varchar(35)
+);
