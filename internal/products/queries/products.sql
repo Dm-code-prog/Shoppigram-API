8c697859c806e25eec78754398a8ca3fa2834ea2
Dm-code-prog <dmitrykozlov504@gmail.com>
2024-05-01 13:56:26 +0400

    Add categories to products

--- internal/products/queries/products.sql
+++ internal/products/queries/products.sql
@@ -3,9 +3,10 @@ select w.name as web_app_name,
        p.id,
        p.name,
        p.description,
+       p.category,
        p.price,
        p.price_currency,
        p.image_url
 from web_apps w
-join products p on w.id = p.web_app_id
+         join products p on w.id = p.web_app_id
 where w.id = $1;
