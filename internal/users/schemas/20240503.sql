0efe5ba9f4095683b93e2a6837eb3778a819dfaa
Dm-code-prog <dmitrykozlov504@gmail.com>
2024-05-03 19:41:33 +0400

    Set max length of first_name and last_name to 255

--- internal/users/schemas/20240503.sql
+++ internal/users/schemas/20240503.sql
@@ -1,3 +1,3 @@
-alter table telegram_users alter column first_name type varchar(64);
-alter table telegram_users alter column last_name type varchar(64);
+alter table telegram_users alter column first_name type varchar(255);
+alter table telegram_users alter column last_name type varchar(255);
 alter table telegram_users alter column username type varchar(32);
