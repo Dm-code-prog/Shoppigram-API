96f0aaeda01624f2071de04bc7da09e2d5a68fed
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-05-10 18:52:59 +0400

    rename marketplaces to admins

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	marketplaces "github.com/shoppigram-com/marketplace-api/internal/admins"
+	"github.com/shoppigram-com/marketplace-api/internal/admins"
 	"github.com/shoppigram-com/marketplace-api/internal/logging"
 	"github.com/shoppigram-com/marketplace-api/internal/notifications"
 	"github.com/shoppigram-com/marketplace-api/internal/orders"
@@ -97,9 +97,9 @@ func main() {
 	ordersService := orders.New(ordersRepo, log.With(zap.String("service", "orders")))
 	ordersHandler := orders.MakeHandler(ordersService, authMw)
 
-	marketplacesRepo := marketplaces.NewPg(db)
-	marketplacesService := marketplaces.New(marketplacesRepo, log.With(zap.String("service", "admins")))
-	marketplacesHandler := marketplaces.MakeHandler(marketplacesService, authMw)
+	adminsRepo := admins.NewPg(db)
+	adminsService := admins.New(adminsRepo, log.With(zap.String("service", "admins")))
+	adminsHandler := admins.MakeHandler(adminsService, authMw)
 
 	notificationsRepo := notifications.NewPg(db, config.Encryption.Key, config.OrderNotifications.BatchSize)
 	notificationsService := notifications.New(
@@ -115,7 +115,7 @@ func main() {
 	r.Mount("/api/v1/public/products", productsHandler)
 	r.Mount("/api/v1/public/auth", tgUsersHandler)
 	r.Mount("/api/v1/public/orders", ordersHandler)
-	r.Mount("/api/v1/private/marketplaces/", marketplacesHandler)
+	r.Mount("/api/v1/private/marketplaces/", adminsHandler)
 
 	g.Add(func() error {
 		log.Info("starting HTTP server", zap.String("port", config.HTTP.Port))
