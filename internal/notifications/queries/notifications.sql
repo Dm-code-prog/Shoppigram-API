d26783eee8d3cb5dce1b2ecd3e79ff525ad56ecc
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-05-17 16:13:49 +0400

    make the notifications sequence more stable in case of the same date

--- internal/notifications/queries/notifications.sql
+++ internal/notifications/queries/notifications.sql
@@ -40,7 +40,7 @@ from orders_batch
          join products p on p.id = op.product_id
          join telegram_users u on external_user_id = u.external_id
          join web_apps wa on orders_batch.web_app_id = wa.id
-order by orders_batch.created_at;
+order by orders_batch.created_at, orders_batch.order_id;
 
 -- name: GetNotificationsForNewMarketplacesAfterCursor :many
 with marketplaces_batch as (select wa.id,
@@ -61,4 +61,4 @@ select marketplaces_batch.id,
 from marketplaces_batch
          join telegram_users u
               on marketplaces_batch.owner_external_id = u.external_id
-order by marketplaces_batch.created_at;
+order by marketplaces_batch.created_at, marketplaces_batch.id;
