a44ddd7da7102b2c4b2fc359514940e67dd0724c
Gleb Guglya <36086974+MrOnimus@users.noreply.github.com>
2024-05-30 22:32:50 +0400

    add DML for getting a user-owned list of channels

--- internal/admins/queries/telegram_channels.sql
+++ internal/admins/queries/telegram_channels.sql
@@ -3,3 +3,11 @@ insert into telegram_channels ( external_id, title, name, is_public, owner_exter
 values ($1, $2, $3, $4, $5)
 on conflict (external_id) do update
 set title = $2, name = $3, is_public = $4, owner_external_id = $5;
+
+-- name: GetTelegramChannels :many
+select id,
+       external_id,
+       name,
+       title
+from telegram_channels
+where owner_external_id = $1;
