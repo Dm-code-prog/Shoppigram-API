8b0e26968f28a730ae958ddb8d2656d277e4a23d
Dm-code-prog <dmitrykozlov504@gmail.com>
2024-09-09 23:45:58 +0300

    - Move the task-definition.json file to /aws/ecs/production directory - Move health package to /packages

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/shoppigram-com/marketplace-api/health"
+	"github.com/shoppigram-com/marketplace-api/packages/health"
 	"net/http"
 	"os"
 	"strings"
